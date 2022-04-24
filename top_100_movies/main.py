db4856178c64c70d6543258310ef25d196a5df14
Denys Pankratov <den4ikvaper@gmail.com>
2022-04-25 00:23:00 +0300

    25.04.2022

---
+++ top_100_movies/main.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in all_movies]
+for n in range(len(movie_titles)-1, -1, -1):
+    print(movie_titles[n])
+    with open("movies.txt", mode="a") as file:
+        file.write(f"{movie_titles[n]}\n")
+
+
+
+
+
